2e64c83c6ce281f1b7d62ce5d981d02df2afef06
amandeeprikhi <amandeeprikhi@gmail.com>
2018-12-31 20:26:27 +0530

    modifying app.js

--- app.js
+++ app.js
@@ -6,22 +6,53 @@ const app = express();
 // get all todos
 
 var workbook = new Excel.Workbook();
-workbook.xlsx.readFile("test-file2.xlsx").then(function () {
-    var worksheet = workbook.getWorksheet(1);
-    worksheet.eachRow(function(row, rowNumber) {
-        // worksheet.getCell(rowNumber, 10).value = worksheet.getCell(rowNumber, 10).value.toString;
-        // console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
-        row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
-            // worksheet.getCell(rowNumber, 10).value = cell.text;
-            console.log('Cell ' + colNumber + ' = ' + cell.type);
+// workbook.xlsx.readFile('test-file.xlsx').then(function () {
+//     var worksheet = workbook.getWorksheet(1);
+//     worksheet.eachRow(function(row, rowNumber) {
+//         // let textdata = worksheet.getCell(rowNumber,10).value.toString();
+//         // worksheet.getCell(rowNumber, 10).value = textdata;
+//         // console.log(JSON.stringify(row.values));
+//     });
+//     worksheet.commit;
+//     workbook.commit;
+//     workbook.csv.writeFile('test-file2.csv').then(function(){
+//         var worksheet2 = workbook.addWorksheet('test');
+//         var rows = [];
+//         for(let index = 1; index <= worksheet.lastRow.number; index ++){
+//             // worksheet2.addRow(worksheet.getRow(index).values);
+//             let textdata = worksheet.getCell(index,10).value.toString();
+//             console.log(JSON.stringify(textdata));
+//             worksheet.getCell(index, 10).value = textdata;
+//             worksheet2.addRow(worksheet.getRow(index).values).commit();
+//             // worksheet2.commit();
+//             // console.log(rows);
+//         }
+        
+//         for(let index = 1; index <= worksheet.lastRow.number; index++){
+//             worksheet2.getRow(index).commit;
+//             // console.log(worksheet2.getRow(index).commit);
+//         }
+//         // console.log()
+//         // console.log(worksheet.properties)
+//         // console.log(worksheet.lastRow.values);
+//         // console.log("CSV WRITTEN!!!!!")
+//         workbook.commit;
+//         worksheet2.commit;
+//     })
+
+    workbook.csv.readFile('test-file2.csv').then(function(){
+        var worksheet3 = workbook.getWorksheet(1);
+        worksheet3.eachRow(function(row, rowNumber){
+            // console.log(JSON.stringify(row.values));
+            row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
+                // worksheet.getCell(rowNumber, 10).value = cell.text;
+                console.log('Cell ' + colNumber + ' = ' + cell.type);
+            });
         });
-    });
-    // workbook.xlsx.writeFile("test-file2.xlsx").then(function(){
-    //     console.log("FILE WRITE SUCCESS");
-    // });
-    // workbook.commit();
-    // return workbook.xlsx.writeFile("test-file2.xlsx");
-});
+    })
+// });
+
+
 
 // workbook.xlsx.readFile("test-file2.xlsx").then(function(){
 //     var worksheet2 = workbook.getWorksheet(1);
