9a717893b6bd03a02fc8221a34d772bf7197a0f9
amandeeprikhi <amandeeprikhi@gmail.com>
2019-01-02 19:12:10 +0530

    structuring API

--- app.js
+++ app.js
@@ -1,110 +1,32 @@
 import express from 'express';
-var Excel = require('exceljs');
-import db from './db/db';
-// Set up the express app
+import excel from './app/excel';
+// import { read } from 'fs';
 const app = express();
-// get all todos
 
-var workbook = new Excel.Workbook();
-// workbook.xlsx.readFile('test-file.xlsx').then(function () {
-//     var worksheet = workbook.getWorksheet(1);
-//     worksheet.eachRow(function(row, rowNumber) {
-//         // let textdata = worksheet.getCell(rowNumber,10).value.toString();
-//         // worksheet.getCell(rowNumber, 10).value = textdata;
-//         // console.log(JSON.stringify(row.values));
-//     });
-//     worksheet.commit;
-//     workbook.commit;
-//     workbook.csv.writeFile('test-file2.csv').then(function(){
-//         var worksheet2 = workbook.addWorksheet('test');
-//         var rows = [];
-//         for(let index = 1; index <= worksheet.lastRow.number; index ++){
-//             // worksheet2.addRow(worksheet.getRow(index).values);
-//             let textdata = worksheet.getCell(index,10).value.toString();
-//             console.log(JSON.stringify(textdata));
-//             worksheet.getCell(index, 10).value = textdata;
-//             worksheet2.addRow(worksheet.getRow(index).values).commit();
-//             // worksheet2.commit();
-//             // console.log(rows);
-//         }
-        
-//         for(let index = 1; index <= worksheet.lastRow.number; index++){
-//             worksheet2.getRow(index).commit;
-//             // console.log(worksheet2.getRow(index).commit);
-//         }
-//         // console.log()
-//         // console.log(worksheet.properties)
-//         // console.log(worksheet.lastRow.values);
-//         // console.log("CSV WRITTEN!!!!!")
-//         workbook.commit;
-//         worksheet2.commit;
-//     })
-
-    workbook.csv.readFile('test-file2.csv').then(function(){
-        var worksheet3 = workbook.getWorksheet(1);
-        worksheet3.eachRow(function(row, rowNumber){
-            // console.log(JSON.stringify(row.values));
-            row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
-                // worksheet.getCell(rowNumber, 10).value = cell.text;
-                console.log('Cell ' + colNumber + ' = ' + cell.type);
-            });
-        });
-    })
-// });
-
-
-
-// workbook.xlsx.readFile("test-file2.xlsx").then(function(){
-//     var worksheet2 = workbook.getWorksheet(1);
-//     worksheet2.eachRow(function(row, rowNumber) {
-//         // worksheet.getCell(rowNumber, 10).value = worksheet.getCell(rowNumber, 10).value.toString;
-//         console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
-//         row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
-//             // worksheet.getCell(rowNumber, 10).value = cell.text;
-//             console.log('Cell ' + colNumber + ' = ' + cell.type);
-//         });
-//     });
-// });
-
-// var sheet1 = workbook.addWorksheet('Sheet1');
-// var row = sheet1.getRow(1);
-// var reColumns = [{
-//         header: 'FirstName',
-//         key: 'firstname'
-//     },
-//     {
-//         header: 'LastName',
-//         key: 'lastname'
-//     },
-//     {
-//         header: 'TEST',
-//         key: 'othername'
-//     }
-// ];
-// sheet1.columns = reColumns;
-// for (let index = 2; index < 5; index++) {
-//     sheet1.getCell('B'+index).value = 2014; 
-// }
-// sheet1.getCell('G6').value='test';
-// workbook.xlsx.writeFile("test-file2.xlsx").then(function () {
-//     console.log("xlsx file is written.");
-//     sheet1.eachRow({ includeEmpty: true }, function(row, rowNumber) {
-//         console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
-//         row.eachCell({ includeEmpty: true }, function(cell, colNumber) {
-//             // cell.value = cell.get;
-//             console.log('Cell ' + colNumber + ' = ' + cell.type);
-//         });
-//     });
-//    console.log(sheet1.getCell(6,7).value);
-//    console.log(sheet1.rowCount);
-// });
 app.get('/api', (req, res) => {
     res.status(200).send({
         success: 'true',
-        message: 'todos retrieved successfully',
-        todos: db
+        message: 'CSV file written!',
+        excel: excel.excel()
     })
 });
+
+app.get('/read', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'EXCEL file read!',
+        excel: excel.read()
+    })
+});
+
+app.get('/write', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'CSV file created!',
+        excel: excel.write()
+    })
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
---
+++ app/excel.js
@@ -0,0 +1,83 @@
+var Excel = require('exceljs');
+
+var excel = function () {
+    let workbook = new Excel.Workbook();
+    workbook.xlsx.readFile('test-file.xlsx').then(function () {
+        let worksheet = workbook.getWorksheet(1);
+        let worksheet2 = workbook.addWorksheet('test');
+        for (let index = 1; index <= worksheet.lastRow.number; index++) {
+            let textdata = worksheet.getCell(index, 10).value.toString();
+            if (textdata.length <= 3) {
+                let textdata2 = textdata + "00";
+                worksheet.getCell(index, 10).value = textdata2;
+                worksheet2.addRow(worksheet.getRow(index).values).commit();
+                workbook.commit;
+            } else if (textdata.length <= 4) {
+                let textdata3 = textdata + "0";
+                worksheet.getCell(index, 10).value = parseInt(textdata3);
+                worksheet2.addRow(worksheet.getRow(index).values).commit();
+                workbook.commit;
+            } else {
+                if (textdata.charAt(5) == "-") {
+                    let part1 = textdata.substring(0, 5);
+                    let part2 = textdata.substring(6, textdata.length);
+                    textdata = part1 + part2;
+                }
+                worksheet.getCell(index, 10).value = textdata;
+                worksheet2.addRow(worksheet.getRow(index).values).commit();
+                workbook.commit;
+            }
+        }
+        workbook.csv.writeFile('test-file.csv').then(function () {
+            console.log("FILE IS WRITTEN!!!!!!!!!!!");
+        });
+    });
+}
+
+var read = function () {
+    let workbook = new Excel.Workbook();
+    workbook.xlsx.readFile('TU Template Sheet.xlsx').then(function () {
+        let worksheet = workbook.getWorksheet(1);
+        worksheet.eachRow(function (row, rowNumber) {
+            console.log(JSON.stringify(row.values))
+            row.eachCell(function (cell, colNumber) {});
+        });
+    })
+}
+
+var write = function () {
+    let workbook = new Excel.Workbook();
+    let workbook2 = new Excel.Workbook();
+    workbook.csv.readFile('TU Template Sheet.csv').then(function () {
+        let worksheet = workbook.getWorksheet(1);
+        worksheet.getRow(1).values;
+        workbook.csv.writeFile('TU Template Sheet.csv').then(function () {
+            workbook2.csv.readFile('test-file.csv').then(function () {
+                let worksheet2 = workbook2.getWorksheet(1);
+                // workbook2.eachSheet(function(worksheet, sheetId) {
+                //     console.log(sheetId);
+                // });
+                for (let index = 2; index <= worksheet2.lastRow.number; index++) {
+                    // console.log(worksheet2.getCell(index,2).value);
+                    worksheet.getCell(index, 1).value = worksheet2.getCell(index, 2).value;
+                    worksheet.getCell(index, 2).value = worksheet2.getCell(index, 3).value;
+                    worksheet.getCell(index, 3).value = worksheet2.getCell(index, 11).value;
+                    worksheet.getCell(index, 7).value = worksheet2.getCell(index, 5).value.toString() + worksheet2.getCell(index, 6).value.toString();
+                    worksheet.getCell(index, 10).value = worksheet2.getCell(index, 7).value;
+                    worksheet.getCell(index, 11).value = worksheet2.getCell(index, 8).value;
+                    worksheet.getCell(index, 12).value = worksheet2.getCell(index, 9).value;
+                    worksheet.addRow(worksheet.getRow(index).values).commit();
+                    console.log("FILE WRITTEN!!!")
+                }
+            })
+            workbook.csv.writeFile('TU Template Sheet2.csv').then(function () {
+                console.log("THE FILE IS CREATED")
+            })
+        })
+    })
+}
+module.exports = {
+    excel: excel,
+    read: read,
+    write: write
+};
